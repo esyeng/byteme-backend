50ea172304b979bf21708f5344b1c74a1e7d9962
Esme Y <61486616+esyeng@users.noreply.github.com>
2023-10-16 21:09:40 -0400

    initializes server methods module, simplifies server.ts

--- server.ts
+++ server.ts
@@ -1,57 +1,30 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const app = express();
-require('dotenv').config();
+import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import { cors } from '@elysiajs/cors';
+import { chatGPT } from './methods';
 
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api", express.static(import.meta.dir + '/api'));
+const port: number = parseInt(process.env.PORT || '5000');
+const hostname: string = process.env.BASE_HOST ? process.env.BASE_HOST : 'localhost';
 
-const openaiApiKey = process.env.OPENAI_API_KEY;
-const BASE_URL = process.env.BASE_URL;
+console.log(`🦊 initial values for port: ${port} and hostname: ${hostname}`);
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Authorization': `Bearer ${openaiApiKey}`,
-};
+const app = new Elysia()
+  .onError(({ code, error }) => {
+    console.log(`Elysia Server Error -> ${code}: ${error}`);
+    return new Response(error.toString());
+  })
+  .use(cors())
+  .use(swagger())
+  .listen({
+    port: port,
+    hostname: hostname
+})
 
-interface ChatRequest {
-  body: {
-    text: string;
-    role?: string;
-    temperature?: number;
-    max_tokens?: number;
-  };
- }
+console.log(`🦊 Elysia is running at ${app.server?.hostname} on port ${app.server?.port}...`);
 
-app.post("/api/chat", async (req: ChatRequest, res: any) => {
-  try {
-    const input: string = req.body ? req.body.text ? req.body.text : "nothing substantial": "eh";
-    const role: string = req.body ? req.body.role ? req.body.role: "user": "user";
-    const response = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
-      {
-        model: 'gpt-3.5-turbo',
-        messages: [
-          {role: 'system', content: 'You are a sassy assistant.'},
-          {role: `${role}`, content: `${input}`},
-        ],
-      },
-      {headers},
-    );
-    const chatGptResponse = response.data.choices[0].message.content;
-    console.log("GPT Response:", chatGptResponse);
-    res.status(200).json({ message: chatGptResponse });
+app
+  .group("/v1", app => app)
+  .get("/", () => `Using v1`)
+    .group("/api", app => app
+      .post("/chat", () => chatGPT));
 
-  } catch (err) {
-    console.log('Error: ' + err);
-    res.status(500).json({ error: 'An error occurred while processing your request' });
-  }
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
