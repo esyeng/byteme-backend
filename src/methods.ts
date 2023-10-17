462299ecdbe26b543eb3ebce4ae7f8933d9b01af
Esme Y <61486616+esyeng@users.noreply.github.com>
2023-10-17 02:13:25 -0400

    relocate TS to source

---
+++ src/methods.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+/**
+ * Contains server methods to be called in server.
+ * @remarks
+ * This file is part of the project named "SassGPT".
+ */
+
+/**
+ * chatGPT is a method that uses the OpenAI API to generate a response to a given input.
+ * @param req - Request object with input & role to be used to generate a chatgpt response.
+ * @param res - Response object to be returned.
+ * @returns A response object with the generated response.
+ */
+
+import { headers } from './server';
+
+export const chatGPT = async (req: any, res: any) => {
+  try {
+    const input: string = req.body ? req.body.text ? req.body.text : "nothing substantial": "eh";
+    const role: string = req.body ? req.body.role ? req.body.role: "user": "user";
+    const response = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {role: 'system', content: `The following is a conversation with an AI personal assistant. The assistant is sassy, creative, clever, and very spicy.\n\n${role}: ${input}\nAI:`},
+          {role: `${role}`, content: `${input}`},
+        ],
+      },
+      {headers},
+    );
+    const chatGptResponse = response.data.choices[0].message.content;
+    console.log("GPT Response:", chatGptResponse);
+    res.status(200).json({ message: chatGptResponse });
+
+  } catch (err) {
+    console.log('Error: ' + err);
+    res.status(500).json({ error: 'An error occurred while processing your request' });
+  }
+}
+
---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import { cors } from '@elysiajs/cors';
+import { chatGPT } from './methods';
+
+export const openaiApiKey = process.env.OPENAI_API_KEY;
+export const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${openaiApiKey}`,
+};
+
+const port: number = parseInt(process.env.PORT || '8080');
+const hostname: string = process.env.NODE_ENV === 'production' ? 'infinite-mesa-72148-b9fabfeefb78.herokuapp.com' : 'localhost';
+
+console.log(`🦊 initial values for port: ${port} and hostname: ${hostname}`);
+console.log(`Process var: ${process.env.BASE_HOST}`);
+
+
+const app = new Elysia()
+  .onError(({ code, error }) => {
+    console.log(`Elysia Server Error -> ${code}: ${error}`);
+    return new Response(error.toString());
+  })
+  .use(cors())
+  .use(swagger())
+  .listen({
+    port: port,
+    hostname: hostname
+})
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname} on port ${app.server?.port}...`);
+
+const defaultEntry = async (req: any, res: any) => {
+  console.log('defaultEntry', req, res);
+  try {
+    res.json({message: `Hello from the one! Bun is fun.`, request: req, response: res});
+  } catch (err) {
+    console.log('Error message: ' + err);
+    res.error(err);
+  }
+}
+
+app.get("/", () => defaultEntry);
+
+app
+  .group("/v1", app => app)
+  // .get("/", () => defaultEntry)
+    .group("/api", app => app
+      .post("/chat", () => chatGPT));
+
