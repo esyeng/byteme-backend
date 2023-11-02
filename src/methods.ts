4c9fd746064bd9d761b91de8334eeab505cb4deb
Esme Y <61486616+esyeng@users.noreply.github.com>
2023-11-01 20:45:43 -0400

    match more complete completion request system with directed route

--- src/methods.ts
+++ src/methods.ts
@@ -6,8 +6,8 @@ import sys from "../.sys";
 import { headers } from "./server";
 
 interface Chat {
-	role: string;
-	content: string;
+    role: string;
+    content: string;
 }
 
 /**
@@ -16,102 +16,38 @@ interface Chat {
  * @param req - Request object with input & role to be used to generate a chatgpt response. Expect role: sring; message: string;
  * @returns A response object with the generated response.
  */
+// The incoming messages are an array of chat objects with a message and from tag
 
-export const callByteMe = async (req: any) => {
-	const { role, message } = req?.body || "";
-	const byteme = sys.messages["byteme_default"];
-	if (!role || !message) {
-		console.log("No message or role provided");
-		return new Error("No text or role provided");
-	}
-	try {
-		const response = await axios.post(
-			"https://api.openai.com/v1/chat/completions",
-			{
-				model: "gpt-4",
-				messages: [
-					{ role: byteme.role, content: byteme.message },
-					{ role: `${role}`, content: `${message}` },
-				],
-			},
-			{ headers }
-		);
-		if (!response || !response.data || !response.data.choices) {
-			console.log("No response from GPT");
-			return new Error("An error occurred while processing your request");
-		} else {
-			const gptResponse = response.data.choices[0].message.content;
-			return { message: gptResponse };
-		}
-	} catch (error) {
-		console.log("Error:", error);
-		return { message: error };
-	}
-};
-
-/**
- * @method callGPT4
- * Calls the GPT-4 model to generate a response based on the given messages.
- * @param req - The request parameters containing the message keys to include in the GPT-4 model input.
- * @returns An object containing the generated response message.
- * @throws An error if there was a problem processing the request.
- */
-/**
-
- *
- * @returns {Chat[]} An array of messages.
- */
-const _stackInstructions = (msgs: string[]): Chat[] => {
-	const messages: Chat[] = [];
-    console.log("started stacking instructions")
-	for (let i = 0; i < msgs.length; i++) {
-		let msg = msgs[i];
-        console.log("msg var", msg)
-		if (sys.messages[msg]) {
-			messages.push({
-				role: sys.messages[msg].role,
-				content: sys.messages[msg].message,
-			});
-		}
-	}
-    console.log(messages)
-	return messages;
-};
-
-export const callGPT4 = async (req: any) => {
-	const { role, message } = req?.body || "";
-    console.log("role and message", role + " " + message)
-	const { msgKeys } = req?.body || [];
-    console.log("message keys", msgKeys)
-    let messages: Chat[] = [];
-    if (msgKeys.length === 0) {
-        messages.push({
-            role: role,
-            content: message
-        });
-    } else {
-        messages = _stackInstructions(msgKeys).concat([{role: role, content: message}]);
+export const byteMe = async (req: any) => {
+    const { messages, id, temperature, model } = req?.body;
+    const byteme = sys.messages["byteme_default"];
+    if (!messages || messages.length === 0) {
+        console.log("No messages received");
+        return new Error("No messages received");
     }
-
-	try {
-		const response = await axios.post(
-			"https://api.openai.com/v1/chat/completions",
-			{
-				model: "gpt-4",
-				messages: messages,
-			},
-			{ headers }
-		);
-		if (!response || !response.data || !response.data.choices) {
-			console.log("No response from GPT");
-			throw new Error("An error occurred while processing your request");
-		} else {
-			const chatGptResponse = response.data.choices[0].message.content;
-			return { message: chatGptResponse };
-		}
-	} catch (error) {
-		return { message: error };
-	}
+    try {
+        const response = await axios.post(
+            "https://api.openai.com/v1/chat/completions",
+            {
+                model: model,
+                messages: [{ role: byteme.role, content: byteme.message }, ...messages],
+                temperature: temperature
+            },
+            { headers }
+        );
+        if (!response || !response.data || !response.data.choices) {
+            console.log("No response from GPT");
+            return new Error("An error occurred while processing your request");
+        } else {
+            const gptResponse = response.data.choices[0].message.content;
+            return { message: gptResponse };
+        }
+    } catch (error) {
+        console.log("Error:", error);
+        return { message: error };
+    }
 };
 
-export const cleanJson = (data: any) => JSON.stringify(data).replace(/[\\r\\n\\t ]+/g, ' ')
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,20 @@
  * @file This file contains the server configuration and routes for Elysia HTTP server.
  * @summary The server listens on a specified port and hostname, and handles GET and POST requests to various endpoints.
  * @description The server uses the Elysia framework, which provides error handling, CORS, and Swagger documentation.
- * The server also uses the OpenAI API to generate responses to user input, using the `callByteMe` and `callGPT4` methods.
+ * The server also uses the OpenAI API to generate responses to user input, using the `byteMe` and `callGPT4` methods.
  * The server exports the `openaiApiKey` and `headers` constants, which are used to authenticate with the OpenAI API.
  * The server exports a default entry function, which returns a message when the root endpoint is accessed.
  */
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { cors } from "@elysiajs/cors";
-import { callByteMe, callGPT4, cleanJson } from "./methods";
+import { byteMe } from "./methods";
 import { auth } from "./auth";
 
 export const openaiApiKey = process.env.OPENAI_API_KEY;
 export const headers = {
-  "Content-Type": "application/json",
-  Authorization: `Bearer ${openaiApiKey}`,
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${openaiApiKey}`,
 };
 
 console.log(`🦊 Before server init`);
@@ -24,54 +24,48 @@ console.log(`🦊 Before server init`);
  *
  * @function app Elysia server instance
  * @method defaultEntry is a method that returns a message when the root endpoint is accessed.
- * @method callByteMe is a method that uses the OpenAI API to generate a response to a given input using the byteme_default system message.
+ * @method byteMe is a method that uses the OpenAI API to generate a response to a given input using the byteme_default system message.
  * @method callGPT4 is a method that uses the OpenAI API to generate a response to a given input
  */
 
 
 const app = new Elysia()
-  .onError(({ code, error }) => {
-    console.log(`Elysia Server Error -> ${code}: ${error}`);
-    return new Response(error.toString());
-  })
-  .get("/", () => "hello elysia.")
-  .group("/api", app =>
-    app
-      .get("/", () => defaultEntry)
-      .post("/byteme", async req => {
-        console.log("byteme function called");
-        const response = await callByteMe(req);
-        return { success: true, response };
-      })
-      .post("/gpt4", async req => {
-        console.log("gpt4 function called");
-        const response = await callGPT4(req);
-        return { success: true, response };
-      })
-      .post("/clean", async req => {
-        console.log("clean function called");
-        const resp = cleanJson(req?.body);
-        return { success: true, response: resp };
-      })
-      .get("/test", async req => {
-        console.log("test function called");
-        const message = "Test get request!";
-        return { success: true, response: message };
-      })
-  )
-  .use(cors())
-  .use(swagger())
+    .onError(({ code, error }) => {
+        console.log(`Elysia Server Error -> ${code}: ${error}`);
+        return new Response(error.toString());
+    })
+    .get("/", () => "hello elysia.")
+    .group("/api", app =>
+        app
+            .get("/", () => defaultEntry)
+            .post("/byteme", async req => {
+                console.log("byteme function called");
+                const response = await byteMe(req);
+                return { success: true, response };
+            })
+            .post("/clean", async req => {
+                console.log("clean function called");
+                return { success: true, response: req?.body };
+            })
+            .get("/test", async req => {
+                console.log("test function called");
+                const message = "Test get request!";
+                return { success: true, response: message };
+            })
+    )
+    .use(cors())
+    .use(swagger())
 
-  .listen({
-    port: process.env.PORT || 8080,
-    hostname: process.env.HOST || "0.0.0.0",
-  });
+    .listen({
+        port: process.env.PORT || 8080,
+        hostname: process.env.HOST || "0.0.0.0",
+    });
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname} on port ${app.server?.port}...`
+    `🦊 Elysia is running at ${app.server?.hostname} on port ${app.server?.port}...`
 );
 
 const defaultEntry = () => {
-  console.log("defaultEntry function called");
-  return { message: `Hello from the one! Bun is fun.` };
+    console.log("defaultEntry function called");
+    return { message: `Hello from the one! Bun is fun.` };
 };
